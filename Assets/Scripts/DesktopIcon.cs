fd45c475e1788cd8434f52edc17963618743135e
Schuyler Henry <schenry823@gmail.com>
2021-11-11 18:49:54 -0800

    Desktop Icon Prefab created

--- Assets/Scripts/DesktopIcon.cs
+++ Assets/Scripts/DesktopIcon.cs
@@ -1,19 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(DoubleClickHandler))]
-public class DesktopIcon : MonoBehaviour
+public class DesktopIcon : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    Image image, textContainer;
+    Text label;
+    DoubleClickHandler doubleClickHandler;
+
+    bool isHighlighted = false;
+    Color highlightedColor = new Color32(74, 58, 253, 255);
+
+    [SerializeField] Sprite icon;
+    [SerializeField] string labelText;
+
+    void Awake()
     {
-        
+        doubleClickHandler = GetComponent<DoubleClickHandler>();
+
+        Image[] images = GetComponentsInChildren<Image>();
+        foreach(Image img in images){
+            if(img.gameObject.name == "Icon"){
+                image = img;
+            } else {
+                textContainer = img;
+            }
+        }
+
+        label = GetComponentInChildren<Text>();
+    }
+    void OnEnable()
+    {
+        doubleClickHandler.doubleClicked += OpenWindow;
+    }
+    void Start(){
+        image.sprite = icon;
+        image.color = Color.white;
+        textContainer.color = new Color(0,0,0,0);
+        label.text = labelText;
+    }
+    void OnDisable(){
+        doubleClickHandler.doubleClicked -= OpenWindow;
+    }
+    public void OnPointerDown(PointerEventData eventData){
+        SetHighlighted(true);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void SetHighlighted(bool highlighted){
+        Debug.Log("Highlighted");
+        if(highlighted){
+            image.color = highlightedColor;
+            textContainer.color = highlightedColor;
+        } else {
+            image.color = Color.white;
+            textContainer.color = new Color(0,0,0,0);
+        }
+        isHighlighted = highlighted;
+    }
+
+    void OpenWindow(){
+        Debug.Log("Open window");
     }
 }
