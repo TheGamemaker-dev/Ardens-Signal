874ede58b9793361fd43a23eb95d385143171dab
Schuyler Henry <schenry823@gmail.com>
2022-01-05 14:50:25 -0800

    Play From Start

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class MenuItems
+{
+    [MenuItem("Tools/Play From Start")]
+    static void Play()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Start.unity");
+        EditorApplication.isPlaying = true;
+    }
+}
